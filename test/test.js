9a785becc1611fea5559cfc7dfbdbfdb1a511bae
Joseph Dykstra <josephdykstra@gmail.com>
2016-02-03 22:17:46 -0600

    Simplify tests
    Use stream.PassThrough instead of through2 module
    Use https.get instead of got module
    Make the last test async

--- test/test.js
+++ test/test.js
@@ -1,40 +1,42 @@
-﻿var test = require('tape')
-var through2 = require('through2')
+var test = require('tape')
+var Stream = require('stream')
 var concat = require('concat-stream')
 var fs = require('fs')
-var got = require('got')
+var https = require('https')
 var DataUriStream = require('../index.js')
 
 var dot = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyb' +
 	'lAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
 
-function equals(t, str) {
-	return concat({ encoding: 'string' }, function (data) {
-		t.equals(data, str || dot)
-		t.end()
-	})
+function expected(t, stream, expect) {
+	stream
+		.pipe(DataUriStream())
+		.pipe(concat({ encoding: 'string' }, function (data) {
+			t.equals(data, expect)
+			t.end()
+		}))
 }
 
 test('fs', function (t) {
-	fs.createReadStream(__dirname + '/Red-dot-5px.png')
-		.pipe(DataUriStream())
-		.pipe(equals(t))
+	var stream = fs.createReadStream(__dirname + '/Red-dot-5px.png')
+	expected(t, stream, dot)
 })
 
 test('http', function (t) {
-	got('https://upload.wikimedia.org/wikipedia/commons/3/31/Red-dot-5px.png')
-		.pipe(DataUriStream())
-		.pipe(equals(t))
+	var url = 'https://upload.wikimedia.org/wikipedia/commons/3/31/Red-dot-5px.png'
+	https.get(url, function (res) {
+		expected(t, res, dot)
+	})
 })
 
 test('unknown type', function(t) {
-	var src = new through2()
+	var stream = new Stream.PassThrough()
 	var expect = 'data:application/octet-stream;base64,aGVsbG8uZ29vZGJ5ZS4='
 
-	src
-		.pipe(DataUriStream())
-		.pipe(equals(t, expect))
+	expected(t, stream, expect)
 
-	src.write('hello.')
-	src.end('goodbye.')
+	stream.write('hello.')
+	setTimeout(function () {
+		stream.end('goodbye.')
+	}, 1)
 })
