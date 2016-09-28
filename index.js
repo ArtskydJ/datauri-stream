d02d9e70062b7f25915fb69d02820c2d116cd041
Joseph Dykstra <josephdykstra@gmail.com>
2016-09-27 22:09:03 -0500

    Fix mime type override

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var map = require('through2-map')
 var fileType = require('file-type')
 
-module.exports = function DataUriStream(opts) {
-	var optsMime = opts && opts.mimeType
-	var fallbackMime = 'application/octet-stream'
+var fallbackMime = 'application/octet-stream'
 
+module.exports = function DataUriStream(opts) {
 	return map(function each(chunk, i) {
 		var encoded = chunk.toString('base64')
 
 		if (i === 0) {
 			var type = fileType(chunk)
-			var mime = optsMime || (type && type.mime) || fallbackMime
+			var mime = (opts && typeof opts === 'object' && opts.mime) || (type && type.mime) || fallbackMime
 			encoded = 'data:' + mime + ';base64,' + encoded
 		}
 
--- test/test.js
+++ test/test.js
@@ -8,25 +8,25 @@ var DataUriStream = require('../index.js')
 var dot = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyb' +
 	'lAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
 
-function expected(t, stream, expect) {
+function expected(t, stream, expect, opts) {
 	stream
-		.pipe(DataUriStream())
+		.pipe(DataUriStream(opts))
 		.pipe(concat({ encoding: 'string' }, function (data) {
 			t.equals(data, expect)
 			t.end()
 		}))
 }
 
-test('fs', function (t) {
-	var stream = fs.createReadStream(__dirname + '/Red-dot-5px.png')
-	expected(t, stream, dot)
-})
-
 test('http', function (t) {
 	var url = 'https://upload.wikimedia.org/wikipedia/commons/3/31/Red-dot-5px.png'
 	https.get(url, function (res) {
 		expected(t, res, dot)
 	})
+})
+
+test('fs', function (t) {
+	var stream = fs.createReadStream(__dirname + '/Red-dot-5px.png')
+	expected(t, stream, dot)
 })
 
 test('unknown type', function(t) {
@@ -39,4 +39,17 @@ test('unknown type', function(t) {
 	setTimeout(function () {
 		stream.end('goodbye.')
 	}, 1)
+})
+
+test('custom mime', function(t) {
+	var stream = fs.createReadStream(__dirname + '/Red-dot-5px.png')
+	var expect = dot.replace('image/png', 'herp/derp')
+	var opts = { mime: 'herp/derp' }
+	expected(t, stream, expect, opts)
+})
+
+test('invalid custom mime', function(t) {
+	var stream = fs.createReadStream(__dirname + '/Red-dot-5px.png')
+	var opts = 'herp/derp' // invalid options
+	expected(t, stream, dot, opts)
 })
